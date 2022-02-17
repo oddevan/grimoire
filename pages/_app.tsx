f0b84844c226beaed263e3fd4e8da22c6d797005
Evan Hildreth <me@eph.me>
2022-02-16 22:28:40 -0500

    Add Bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
+import "bootstrap/dist/css/bootstrap.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function GrimoireApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {}, []);
+
   return <Component {...pageProps} />;
 }
 
-export default GrimoireApp
+export default GrimoireApp;
