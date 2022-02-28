d12d91b4dbfa07c352909e20841f6263918cc8d9
Evan Hildreth <slightly@oddevan.com>
2022-02-28 15:40:50 -0500

    Correct build error

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,13 +1,16 @@
 import Navbar from "../components/Navbar";
 import SmolblogProvider from "../contexts/SmolblogProvider";
+import { Fragment } from "react";
 
 export default function MainLayout(props: any) {
 	const { children } = props;
 
 	return (
 		<SmolblogProvider>
-			<Navbar />
-			<main className="container">{children}</main>
+			<Fragment>
+				<Navbar />
+				<main className="container">{children}</main>
+			</Fragment>
 		</SmolblogProvider>
 	);
 }
