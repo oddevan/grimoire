0a8c4e29a20325e1aad333fb851ac3357fdd7ed8
Evan Hildreth <me@eph.me>
2022-03-01 18:52:35 -0500

    Attempting Vault?

--- contexts/SmolblogProvider.tsx
+++ contexts/SmolblogProvider.tsx
@@ -4,16 +4,19 @@ import {
 	InMemoryCache,
 } from "@apollo/client";
 import { createContext, Fragment, useContext, useState } from "react";
+import { Vault } from "@ultimate/vault";
 
 export interface SmolblogContextProps {
 	smolblogAccessCode: string;
 	apolloClient?: ApolloClient<NormalizedCacheObject>;
 
 	setSmolblogCode: Function;
+	logoutSmolblog: Function;
 }
 const SmolblogContext = createContext<SmolblogContextProps>({
 	smolblogAccessCode: "",
 	setSmolblogCode: () => {},
+	logoutSmolblog: () => {},
 });
 
 export default function SmolblogProvider({ children = <Fragment /> }) {
@@ -22,21 +25,40 @@ export default function SmolblogProvider({ children = <Fragment /> }) {
 		new ApolloClient({ cache: new InMemoryCache() })
 	);
 
+	if (!global.window) return children;
+
+	const session = new Vault({ type: "session" });
+
+	const setSmolblogCode = (code: string) => {
+		setSmolblogAccessCode(code);
+		setApolloClient(
+			new ApolloClient({
+				uri: "https://grimoireapp.smolblog.com/graphql/",
+				cache: new InMemoryCache(),
+				headers: { Authorization: `Bearer ${code}` },
+			})
+		);
+		session.set<string>("smolblogUser", code);
+	};
+
+	const logoutSmolblog = () => {
+		setSmolblogAccessCode("");
+		setApolloClient(new ApolloClient({ cache: new InMemoryCache() }));
+		session.removeAll();
+	};
+
+	if (!smolblogAccessCode) {
+		const userKey = session.get<string>("smolblogUser");
+		if (userKey) setSmolblogAccessCode(userKey);
+	}
+
 	return (
 		<SmolblogContext.Provider
 			value={{
 				smolblogAccessCode,
-				setSmolblogCode: (code: string) => {
-					setSmolblogAccessCode(code);
-					setApolloClient(
-						new ApolloClient({
-							uri: "https://grimoireapp.smolblog.com/graphql/",
-							cache: new InMemoryCache(),
-							headers: { Authorization: `Bearer ${code}` },
-						})
-					);
-				},
 				apolloClient,
+				setSmolblogCode,
+				logoutSmolblog,
 			}}
 		>
 			{children}
