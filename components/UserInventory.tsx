65821426806332d35003c92149814d4945d2b811
Evan Hildreth <me@eph.me>
2022-03-06 19:45:28 -0500

    Use REST endpoint to get card collections

--- components/UserInventory.tsx
+++ components/UserInventory.tsx
@@ -1,8 +1,11 @@
-import type { ApolloConsumer } from "@apollo/client";
 import { useSmolblog } from "../contexts/SmolblogProvider";
 import { Fragment, useState } from "react";
 import InventoryLineItem from "./InventoryLineItem";
 import { GrimoireCard } from "../types/GrimoireCard";
+import {
+	CardCollectionLineItem,
+	getUserCollectionsForCard,
+} from "../lib/smolblog/card";
 
 export interface UserInventoryProps {
 	card: GrimoireCard;
@@ -10,25 +13,34 @@ export interface UserInventoryProps {
 
 export default function UserInventory(props: UserInventoryProps) {
 	const { card } = props;
-	const { smolblogAccessCode, apolloClient } = useSmolblog();
-	const [inventoryItems, setInventoryItems] = useState([]);
+	const { smolblogAccessCode } = useSmolblog();
+	const [inventoryItems, setInventoryItems] = useState<
+		[CardCollectionLineItem?]
+	>([]);
 
 	if (!smolblogAccessCode && global.window) {
 		return <Fragment />;
 	}
 
+	getUserCollectionsForCard(smolblogAccessCode, card.id).then((result) =>
+		setInventoryItems(result ?? [])
+	);
+
 	return (
 		<div className="row justify-content-center">
 			<div className="col-md-8">
 				<h2 className="h3">My Inventory</h2>
-				{inventoryItems.map((lineItem) => (
-					<InventoryLineItem
-						key={Math.random()}
-						card={card}
-						collection={lineItem.collection}
-						quantity={lineItem.quantity}
-					/>
-				))}
+				{inventoryItems.map(
+					(lineItem) =>
+						lineItem && (
+							<InventoryLineItem
+								key={Math.random()}
+								card={card}
+								collection={lineItem.collection}
+								quantity={lineItem.quantity}
+							/>
+						)
+				)}
 			</div>
 		</div>
 	);
---
+++ lib/smolblog/card.ts
@@ -0,0 +1,28 @@
+import { GrimoireCollection } from "../../types/GrimoireCollection";
+import { smolblogGetSettings } from "./utils";
+
+export interface CardCollectionLineItem {
+	collection: GrimoireCollection;
+	quantity: number;
+}
+
+export async function getUserCollectionsForCard(smolblogAccessCode: string, cardId: string): Promise<[CardCollectionLineItem?]> {
+	if (!smolblogAccessCode || !cardId) return [];
+	
+	const response = await fetch(
+		`https://grimoireapp.smolblog.com/wp-json/grimoire/v1/card/${cardId}/usercollections`,
+		smolblogGetSettings(smolblogAccessCode),
+	);
+
+	const collectionData = await response.json();
+
+  return collectionData ? collectionData.map((lineItem: any) => {
+		return {
+			collection: {
+				id: lineItem.id,
+				name: lineItem.name,
+			},
+			quantity: lineItem.quantity
+		}
+	}) : [];
+}
