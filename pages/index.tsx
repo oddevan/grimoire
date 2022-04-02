8642052d63fde13d669715cff3a17eeabcb71534
Evan Hildreth <me@eph.me>
2022-04-02 13:20:35 -0400

    Make it better for static rendering

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,21 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from "next/link";
 import dynamic from "next/dynamic";
+import { useSmolblog } from "../contexts/SmolblogProvider";
+import { useEffect, useState } from "react";
 
 const LoginDynamic = dynamic(() => import("../components/SmolblogLogin"), {
 	ssr: false,
 });
 
 const Home: NextPage = () => {
+	const [showLogin, setShowLogin] = useState(false);
+	const { smolblogAccessCode } = useSmolblog();
+
+	useEffect(() => {
+		setShowLogin(!smolblogAccessCode);
+	}, [smolblogAccessCode]);
+
 	return (
 		<div>
 			<Head>
@@ -23,7 +32,13 @@ const Home: NextPage = () => {
 				.
 			</p>
 			<p style={{ textAlign: "center" }}>
-				<LoginDynamic />
+				{showLogin ? (
+					<LoginDynamic />
+				) : (
+					<Link href="/profile">
+						<a>Go to your profile</a>
+					</Link>
+				)}
 			</p>
 		</div>
 	);
