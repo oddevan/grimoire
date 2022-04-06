626bf8d3f51bb40966cc6e937c0e8c44e98019b8
Evan Hildreth <me@eph.me>
2022-04-05 20:42:37 -0400

    Add Card price

---
+++ components/CardPrice.tsx
@@ -0,0 +1,20 @@
+import {useState, Fragment} from 'react';
+import {Alert} from 'react-bootstrap';
+
+export default function CardPrice(params: {id: string}) {
+    const [price, setPrice] = useState<number>(-1);
+    const { id } = params;
+
+    useEffect((() => {
+        fetch(`${process.env.NEXT_PUBLIC_SMOLBLOG_API_BASE}card/${id}/usercollections`)
+            .then(response => response.json())
+            .then(body => setPrice(body.price))
+            .catch(error => console.error(error));
+    }), [id])
+
+    if ( price < 0 ) return <Fragment />;
+
+    return (
+        <Alert variant="success">Today's market price: ${price}</Alert>
+    );
+}
--- pages/cards/[id].tsx
+++ pages/cards/[id].tsx
@@ -6,6 +6,7 @@ import { getAllCardIds, getCardInfo } from "../../lib/static";
 import { GrimoireCard } from "../../types/GrimoireCard";
 import dynamic from "next/dynamic";
 import { Col, Row } from "react-bootstrap";
+import CardPrice from "../../components/CardPrice";
 
 type CardPageParams = {
 	id: string;
@@ -50,6 +51,7 @@ export default function CardPage(card: GrimoireCard) {
 								<dt className="col-lg-6">PokemonTCG.guru ID</dt>
 								<dd className="col-lg-6">{card.guruId}</dd>
 							</dl>
+							<CardPrice id={card.id} />
 						</Col>
 					</Row>
 				</Col>
