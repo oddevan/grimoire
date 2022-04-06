2adb9d93dc7592046d2414be8d74b31654e3500c
Evan Hildreth <me@eph.me>
2022-04-06 19:23:24 -0400

    Formatting changes

--- components/CardPrice.tsx
+++ components/CardPrice.tsx
@@ -1,21 +1,23 @@
-import {useState, Fragment, useEffect} from 'react';
-import {Alert} from 'react-bootstrap';
+import { useState, Fragment, useEffect } from "react";
+import { Alert } from "react-bootstrap";
 
-export default function CardPrice(params: {id: string}) {
-    const [price, setPrice] = useState<number>(-1);
-    const { id } = params;
+export default function CardPrice(params: { id: string }) {
+	const [price, setPrice] = useState<number>(-1);
+	const { id } = params;
 
-    useEffect((() => {
-        setPrice(-1);
-        fetch(`${process.env.NEXT_PUBLIC_SMOLBLOG_API_BASE}card/${id}/usercollections`)
-            .then(response => response.json())
-            .then(body => setPrice(body.price))
-            .catch(error => console.error(error));
-    }), [id])
+	useEffect(() => {
+		setPrice(-1);
+		fetch(
+			`${process.env.NEXT_PUBLIC_SMOLBLOG_API_BASE}card/${id}/usercollections`
+		)
+			.then((response) => response.json())
+			.then((body) => setPrice(body.price))
+			.catch((error) => console.error(error));
+	}, [id]);
 
-    if ( price < 0 ) return <Fragment />;
+	if (price < 0) return <Fragment />;
 
-    return (
-        <Alert variant="success">Today&apos;s market price: {`\$${price}`}</Alert>
-    );
+	return (
+		<Alert variant="success">Today&apos;s market price: {`\$${price}`}</Alert>
+	);
 }
